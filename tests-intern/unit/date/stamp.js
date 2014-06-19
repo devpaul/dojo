5c82486db8f5e28ce5a5b283178858e56f8442ab
devpaul <pshannon@sitepen.com>
2014-06-19 13:25:22 -0700

    conformed to style

--- tests-intern/unit/date/stamp.js
+++ tests-intern/unit/date/stamp.js
@@ -11,7 +11,7 @@ define([
 			// [RFC3339](http://www.ietf.org/rfc/rfc3339.txt), except that partial input is allowed.
 			'ISO8601 & RFC3339': {
 				'date and time with timezone offset': function () {
-					var rfc = "2005-06-29T08:05:00-07:00";
+					var rfc = '2005-06-29T08:05:00-07:00';
 					var date = stamp.fromISOString(rfc);
 					assert.equal(2005, date.getFullYear());
 					assert.equal(5, date.getMonth());
@@ -22,7 +22,7 @@ define([
 				},
 
 				'date only': function () {
-					var rfc = "2004-02-29";
+					var rfc = '2004-02-29';
 					var date = stamp.fromISOString(rfc);
 					assert.equal(2004, date.getFullYear());
 					assert.equal(1, date.getMonth());
@@ -30,7 +30,7 @@ define([
 				},
 
 				'year and month only': function () {
-					var rfc = "2004-01";
+					var rfc = '2004-01';
 					var date = stamp.fromISOString(rfc);
 					assert.equal(2004, date.getFullYear());
 					assert.equal(0, date.getMonth());
@@ -39,7 +39,7 @@ define([
 
 				'date and time without timezone': function () {
 					// No TZ info means local time
-					var rfc = "2004-02-29T01:23:45";
+					var rfc = '2004-02-29T01:23:45';
 					var date = stamp.fromISOString(rfc);
 					assert.equal(2004, date.getFullYear());
 					assert.equal(1, date.getMonth());
@@ -48,7 +48,7 @@ define([
 				},
 
 				'pre-epoch date': function () {
-					var rfc = "0101-01-01";
+					var rfc = '0101-01-01';
 					var date = stamp.fromISOString(rfc);
 					assert.equal(101, date.getFullYear());
 					assert.equal(0, date.getMonth());
@@ -56,13 +56,13 @@ define([
 				},
 
 				'pre-epoch date and time': function () {
-					var rfc = "0001-01T00:00:00";
+					var rfc = '0001-01T00:00:00';
 					var date = stamp.fromISOString(rfc);
 					assert.equal(1, date.getFullYear());
 				},
 
 				'time only': function () {
-					var date = stamp.fromISOString("T18:46:39");
+					var date = stamp.fromISOString('T18:46:39');
 					assert.equal(18, date.getHours());
 					assert.equal(46, date.getMinutes());
 					assert.equal(39, date.getSeconds());
@@ -72,27 +72,27 @@ define([
 			// process dates as specified [by the W3C](http://www.w3.org/TR/NOTE-datetime)
 			'w3 date-time': {
 				'timestamp only with timezone offset': function() {
-					var date = stamp.fromISOString("T18:46:39+07:00");
+					var date = stamp.fromISOString('T18:46:39+07:00');
 					assert.equal(11, date.getUTCHours());
 				},
 
 				'timestamp only with 0 timezone offset and + designator': function () {
-					var date = stamp.fromISOString("T18:46:39+00:00");
+					var date = stamp.fromISOString('T18:46:39+00:00');
 					assert.equal(18, date.getUTCHours());
 				},
 
 				'timestamp only with timezone desiginator and no offset': function () {
-					var date = stamp.fromISOString("T18:46:39Z");
+					var date = stamp.fromISOString('T18:46:39Z');
 					assert.equal(18, date.getUTCHours());
 				},
 
 				'timestamp only with timzone offset with - designator': function () {
-					var date = stamp.fromISOString("T16:46:39-07:00");
+					var date = stamp.fromISOString('T16:46:39-07:00');
 					assert.equal(23, date.getUTCHours());
 				},
 
 				'GMT timestamp only with timezone designator, no offset': function () {
-					var date = stamp.fromISOString("T00:00:00Z", new Date(2010,3,1));
+					var date = stamp.fromISOString('T00:00:00Z', new Date(2010,3,1));
 					assert.equal(0, date.getUTCHours());
 					assert.equal(2010, date.getFullYear());
 				}
@@ -104,7 +104,7 @@ define([
 				var date = new Date(2005, 5, 29, 8, 5, 0);
 				var rfc = stamp.toISOString(date);
 				//truncate for comparison
-				assert.equal("2005-06", rfc.substring(0, 7));
+				assert.equal('2005-06', rfc.substring(0, 7));
 			},
 
 			'pre-epoch year': function () {
@@ -114,7 +114,7 @@ define([
 				date.setFullYear(101);
 				rfc = stamp.toISOString(date);
 				//truncate for comparison
-				assert.equal("0101-01", rfc.substring(0, 7));
+				assert.equal('0101-01', rfc.substring(0, 7));
 			}
 		}
 	});
