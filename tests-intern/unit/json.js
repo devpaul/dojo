6d2e27d7c9a45b4a720e18edfece420076661cbd
devpaul <pshannon@sitepen.com>
2014-06-20 12:37:44 -0700

    fixed JSON usage and made assertions actual, expected

--- tests-intern/unit/json.js
+++ tests-intern/unit/json.js
@@ -8,146 +8,149 @@ define([
 
 		'.parse': {
 			'simple string': function () {
-				assert.deepEqual({ foo: 'bar' }, JSON.parse('{"foo":"bar"}'));
+				assert.deepEqual(json.parse('{"foo":"bar"}'), { foo: 'bar' });
 			},
 
 			'simple true': function () {
-				assert.deepEqual({ foo: true }, JSON.parse('{"foo":true}'));
+				assert.deepEqual(json.parse('{"foo":true}'), { foo: true });
 			},
 
 			'simple false': function () {
-				assert.deepEqual({ foo: false }, JSON.parse('{"foo":false}'));
+				assert.deepEqual(json.parse('{"foo":false}'), { foo: false });
 			},
 
 			'simple null': function () {
-				assert.deepEqual({ foo: null }, JSON.parse('{"foo":null}'));
+				assert.deepEqual(json.parse('{"foo":null}'), { foo: null });
 			},
 
 			'simple number': function () {
-				assert.deepEqual({ foo: 3.3 }, JSON.parse('{"foo":3.3}'));
+				assert.deepEqual(json.parse('{"foo":3.3}'), { foo: 3.3 });
 			},
 
 			'strict string': function () {
-				assert.deepEqual({ foo: 'bar' }, JSON.parse('{"foo":"bar"}', true));
+				assert.deepEqual(json.parse('{"foo":"bar"}', true), { foo: 'bar' });
 			},
 
 			'strict empty string': function () {
-				assert.deepEqual({ foo: '' }, JSON.parse('{"foo":""}', true));
+				assert.deepEqual(json.parse('{"foo":""}', true), { foo: '' });
 			},
 
 			'strict escaped string': function () {
-				assert.deepEqual({ foo: 'b\n\t\"ar()' }, JSON.parse('{"foo":"b\\n\\t\\"ar()"}', true));
+				assert.deepEqual(json.parse('{"foo":"b\\n\\t\\"ar()"}', true), { foo: 'b\n\t\"ar()' });
 			},
 
 			'strict true': function () {
-				assert.deepEqual({ foo: true }, JSON.parse('{"foo":true}', true));
+				assert.deepEqual(json.parse('{"foo":true}', true), { foo: true });
 			},
 
 			'strict false': function () {
-				assert.deepEqual({ foo: false }, JSON.parse('{"foo":false}', true));
+				assert.deepEqual(json.parse('{"foo":false}', true), { foo: false });
 			},
 
 			'strict null': function () {
-				assert.deepEqual({ foo: null }, JSON.parse('{"foo":null}', true));
+				assert.deepEqual(json.parse('{"foo":null}', true), { foo: null });
 			},
 
 			'strict number': function () {
-				assert.deepEqual({ foo: 3.3 }, JSON.parse('{"foo":3.3}', true));
+				assert.deepEqual(json.parse('{"foo":3.3}', true), { foo: 3.3 });
 			},
 
 			'strict negative number': function () {
-				assert.deepEqual({ foo: -3.3 }, JSON.parse('{"foo":-3.3}', true));
+				assert.deepEqual(json.parse('{"foo":-3.3}', true), { foo: -3.3 });
 			},
 
 			'strict negative exponent': function () {
-				assert.deepEqual({ foo: 3.3e-33 }, JSON.parse('{"foo":3.3e-33}', true));
+				assert.deepEqual(json.parse('{"foo":3.3e-33}', true), { foo: 3.3e-33 });
 			},
 
 			'strict exponent': function () {
-				assert.deepEqual({ foo: 3.3e33 }, JSON.parse('{"foo":3.3e33}', true));
+				assert.deepEqual(json.parse('{"foo":3.3e33}', true), { foo: 3.3e33 });
 			},
 
 			'strict array': function () {
-				assert.deepEqual({ foo: [3, true, []] }, JSON.parse('{"foo":[3,true,[]]}', true));
+				assert.deepEqual(json.parse('{"foo":[3,true,[]]}', true), { foo: [3, true, []] });
 			},
 
 			'bad call throws': function () {
 				assert.throws(function () {
-					JSON.parse('{"foo":alert()}', true);
+					json.parse('{"foo":alert()}', true);
 				});
 			},
 
 			'bad math throws': function () {
 				assert.throws(function () {
-					JSON.parse('{"foo":3+4}', true);
+					json.parse('{"foo":3+4}', true);
 				});
 			},
 
 			'bad array index throws': function () {
 				assert.throws(function () {
-					JSON.parse('{"foo":"bar"}[3]', true);
+					json.parse('{"foo":"bar"}[3]', true);
 				});
 			},
 
 			'unquoted object key throws': function () {
 				assert.throws(function () {
-					JSON.parse('{foo:"bar"}', true);
+					json.parse('{foo:"bar"}', true);
 				});
 			},
 
 			'unclosed array throws': function () {
 				assert.throws(function () {
-					JSON.parse('[', true);
+					json.parse('[', true);
 				});
 			},
 
 			'closing an unopened object literal throws': function () {
 				assert.throws(function () {
-					JSON.parse('}', true);
+					json.parse('}', true);
 				});
 			},
 
 			'malformed array throws': function () {
 				assert.throws(function () {
-					JSON.parse('["foo":"bar"]');
+					json.parse('["foo":"bar"]');
 				});
 			}
 		},
 
 		'.stringify': {
 			'string': function () {
-				assert.equal('{"foo":"bar"}', JSON.stringify({"foo":"bar"}));
+				assert.equal(json.stringify({'foo':'bar'}), '{"foo":"bar"}');
 			},
 
 			'null': function () {
-				assert.equal('{"foo":null}', JSON.stringify({"foo":null}));
+				assert.equal(json.stringify({'foo':null}), '{"foo":null}');
 			},
 
 			'function': function () {
-				assert.equal('{}', JSON.stringify({"foo":function(){}}));
+				assert.equal(json.stringify({'foo':function(){}}), '{}');
 			},
 
 			'NaN': function () {
-				assert.equal('{"foo":null}', JSON.stringify({"foo":NaN}));
+				assert.equal(json.stringify({'foo':NaN}), '{"foo":null}');
 			},
 
 			'Infinity': function () {
-				assert.equal('{"foo":null}', JSON.stringify({"foo":Infinity}));
+				assert.equal(json.stringify({'foo':Infinity}), '{"foo":null}');
 			},
 
 			'date': function () {
-				// there is differences in how many decimals of accuracies in seconds in how Dates are serialized between browsers
-				assert.match(JSON.parse(JSON.stringify({"foo":new Date(1)})).foo, /1970-01-01T00:00:00.*Z/);
+				// there is differences in how many decimals of accuracies in seconds in how Dates
+				// are serialized between browsers
+				assert.match(json.parse(json.stringify({'foo':new Date(1)})).foo, /1970-01-01T00:00:00.*Z/);
 			},
 
 			'inherited object': function () {
-				function FooBar() { this.foo = "foo"; }
-				FooBar.prototype.bar = "bar";
-				assert.equal('{"foo":"foo"}', JSON.stringify(new FooBar()));
+				function FooBar() { this.foo = 'foo'; }
+				FooBar.prototype.bar = 'bar';
+				assert.equal(json.stringify(new FooBar()), '{"foo":"foo"}');
 			},
 
 			'toJson': function () {
-				assert.equal('{"foo":{"name":"value"}}', JSON.stringify({foo:{toJSON:function(){return {name:"value"}; }}}));
+				/* jshint unused:false */
+				var obj = {foo:{toJSON:function(){return {name:'value'}; }}};
+				assert.equal(json.stringify(obj), '{"foo":{"name":"value"}}');
 			}
 		}
 	});
