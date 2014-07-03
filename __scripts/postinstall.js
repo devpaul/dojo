c4b69517e6d9e16dc0ad13f15ab854cdcb665f31
devpaul <pshannon@sitepen.com>
2014-07-03 13:37:37 -0700

    adding 'dir' type to postinstall for Windows

--- __scripts/postinstall.js
+++ __scripts/postinstall.js
@@ -3,8 +3,8 @@
 var fs = require('fs');
 
 if(!fs.existsSync('./node_modules/istanbul')) {
-	fs.symlinkSync('./intern-geezer/node_modules/istanbul', './node_modules/istanbul');
+	fs.symlinkSync('./intern-geezer/node_modules/istanbul', './node_modules/istanbul', 'dir');
 }
 if(!fs.existsSync('./node_modules/source-map')) {
-	fs.symlinkSync('./intern-geezer/node_modules/source-map', './node_modules/source-map');
+	fs.symlinkSync('./intern-geezer/node_modules/source-map', './node_modules/source-map', 'dir');
 }
