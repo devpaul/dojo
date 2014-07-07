1dc065cea591dd121cd6e85cc701604e09fa03c9
devpaul <pshannon@sitepen.com>
2014-07-07 13:02:53 -0700

    converted dojo/cache tests from doh to intern (cherry picked from commit 7dd396b)

--- tests-intern/unit/all.js
+++ tests-intern/unit/all.js
@@ -17,6 +17,7 @@ define([
 	'./Stateful',
 	'./currency',
 	'./date',
+	'./cache',
 	// './date/locale',
 	// './date/stamp',
 
--- tests-intern/unit/cache.js
+++ tests-intern/unit/cache.js
@@ -2,113 +2,41 @@ define([
 	'intern!object',
 	'intern/chai!assert',
 	'sinon',
-	'dojo/Deferred',
-	'dojo/_base/kernel',
-	'dojo/has',
+	'dojo/cache',
+	'dojo/_base/url',
 	'dojo/_base/lang'
-], function (registerSuite, assert, sinon, Deferred, dojo, has, lang) {
-	registerSuite(function () {
-		var regular = '<h1>Hello World</h1>';
-		var unsanitized = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"' +
-			'"http://www.w3.org/TR/html4/loose.dtd">' +
-			'<html>' +
-			'<head>' +
-			'	<script type="text/javascript" src="../../dojo.js"></script>' +
-			'	<script type="text/javascript" src="../../cache.js"></script>' +
-			'</head>' +
-			'<body class="tundra">' +
-			'	<h1>Hello World</h1>' +
-			'</body>' +
-			'</html>';
-		var cache;
+], function (registerSuite, assert, sinon, cache, Url, lang) {
+	var expected = '<h1>Hello World</h1>';
+	var cacheNamespace = 'dojo.tests-intern.unit.__support.cache';
+	var cacheSanitizeOptions = { sanitize: true };
 
+	registerSuite(function () {
 		return {
 			name: 'dojo/cache',
 
-			'beforeEach': function () {
-				var dfd = new Deferred();
-
-				require.undef('dojo/cache');
-				require.undef('dojo/text');
-				delete dojo.cache;
-				if(has('host-browser')) {
-					mockGetTextForBrowser().then(requireDojoCache);
-				} else {
-					mockGetTextForNode().then(requireDojoCache);
-				}
-
-				return dfd;
-
-
-				function requireDojoCache() {
-					require(['dojo/cache'], function (_cache) {
-						cache = _cache;
-						dfd.resolve();
-					});
-				}
-
-				function mockGetTextForBrowser() {
-					var dfd = new Deferred();
-
-					has('config-requestProvider');
-					require.undef('dojo/request');
-					require.undef('dojo/request/default!');
-
-					require(['dojo/request/registry'], function (registry) {
-						registry.register(/.*regular\.html$/, function () {
-							var dfd = new Deferred();
-							dfd.resolve(regular);
-							return dfd;
-						});
-						registry.register(/.*sanitized\.html$/, function () {
-							var dfd = new Deferred();
-							dfd.resolve(unsanitized);
-							return dfd;
-						});
-						dfd.resolve();
-					});
-
-					return dfd;
-				}
-
-				function mockGetTextForNode() {
-					// TODO implement
-					throw new Error('implement');
-				}
-			},
-
-			'afterEach': function () {
-				has('config-requestProvider', false);
-				require.undef('dojo/cache');
-				require.undef('dojo/text');
-				require.undef('dojo/request');
-				require.undef('dojo/request/default!');
-			},
-
 			'caches xhr request': function () {
-				var actual = lang.trim(cache('dojo.tests.cache', 'regular.html'));
-				assert.equal(actual, regular);
+				var actual = lang.trim(cache(cacheNamespace, 'regular.html'));
+				assert.equal(actual, expected);
 			},
 
 			'sanatizes request': function () {
-				var actual =lang.trim(cache('dojo.tests.cache', 'sanitized.html', { sanitize: true }));
-				assert.equal(actual, regular);
+				var actual =lang.trim(cache(cacheNamespace, 'sanitized.html', cacheSanitizeOptions));
+				assert.equal(actual, expected);
 			},
 
 			'object variant passed as module': function () {
-
-			},
-
-			'files are loaded only once': function () {
-
+				var objPath = require.toUrl('dojo/tests-intern/unit/__support/cache/object.html');
+				var actual = lang.trim(cache(new Url(objPath), cacheSanitizeOptions));
+				assert.equal(actual, expected);
 			},
 
 			'unset cache returns null and does not throw': function () {
-
+				assert.isNull(cache(cacheNamespace, 'regular.html', null));
 			},
 
 			'set value': function () {
-
+				assert.equal('', cache(cacheNamespace, 'regular.html', ''));
+				assert.equal('', cache(cacheNamespace, 'regular.html'));
 			}
 		};
 	});
